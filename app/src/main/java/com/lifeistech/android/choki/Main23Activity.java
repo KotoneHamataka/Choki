2653c9a7990a62b02114d7ee9fdf11d6d6683f71
Kotone Hamataka <kochiii@hotmail.co.jp>
2016-02-22 21:23:15 +0900

    Initial commit

---
+++ app/src/main/java/com/lifeistech/android/choki/Main23Activity.java
@@ -0,0 +1,21 @@
+package com.lifeistech.android.choki;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main23Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main23);
+    }
+
+
+
+
+
+    public void next(View v){
+
+    }
+}
